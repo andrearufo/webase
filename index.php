c3f3f0444d55123d7c949966dc2ebbaf6bbee470
Andrea Rufo <orangedropdesign@gmail.com>
2015-06-02 15:53:55 +0200

    ver. 0.1

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html class="no-js" lang="it">
+	
+<head>
+	
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title></title>
+	
+	<meta name="description" content="">
+	
+	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+	<link rel="icon" href="images/favicon.png" type="image/png" />
+	
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="style.css" />
+	
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	
+</head>
+
+<body>
+	
+	<!--[if lt IE 8]><p class="browserupgrade alert alert-warning">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
+	
+	<div class="container">
+		<h1>Howdy!</h1>
+		<p>This is you app...</p>
+	</div>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src="js/main.min.js"></script>
+	
+</body>
+
+</html>
